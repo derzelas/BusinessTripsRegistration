0cbd8fdb356e60f12376107ad5e9dd215bb52009
derzelas <daniel.savu@gmail.com>
2015-06-29 11:55:27 +0300

    Initial setup

---
+++ BusinessTrips/BusinessTrips/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessTrips.Startup))]
+namespace BusinessTrips
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
