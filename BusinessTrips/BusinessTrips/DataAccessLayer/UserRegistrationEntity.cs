a11e911a7f1ff39d7bedb10d434fdaf0ebad55d7
Andreea19 <nicoara.andreea19@gmail.com>
2015-06-30 17:03:52 +0300

    Added UserRegistrationEntity class

---
+++ BusinessTrips/BusinessTrips/DataAccessLayer/UserRegistrationEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BusinessTrips.Models;
+
+namespace BusinessTrips.DataAccessLayer
+{
+    public class UserRegistrationEntity
+    {
+        public bool IsConfirmed { get; set; }
+        public UserModels User { get; set; }
+        public Guid Id { get; set; }
+    }
+}
