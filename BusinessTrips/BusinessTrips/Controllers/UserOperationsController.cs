d2675b5c39b554ffc8e62ce27fba8c917a10869d
BotaPetrica <petrica.bota2@yahoo.com>
2015-06-30 16:42:34 +0300

    email_1

--- BusinessTrips/BusinessTrips/Controllers/UserOperationsController.cs
+++ BusinessTrips/BusinessTrips/Controllers/UserOperationsController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Net.Mail;
+using System.Net;
+
 namespace BusinessTrips.Controllers
 {
     public class UserOperationsController : Controller
@@ -11,11 +14,29 @@ namespace BusinessTrips.Controllers
         //
         // GET: /UserOperations/
 
+        public class Email
+        {
+            public Email()
+            {
+            SmtpClient  smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl=true;
+            smtp.UseDefaultCredentials=false;
+
+            smtp.Credentials = new NetworkCredential("user", "parola");
+            MailMessage mail = new MailMessage("sandica_robert@gmail.com", "petrica.bota2@yahoo.com", "Message", "Hello World");
+            smtp.Send(mail);
+
+            }
+            
+        }
         public ActionResult RegisterNewUser()
         {
+            //save in depository
+            //send e mail
+            
             return View();
         }
-
+        
         public ActionResult ConfirmRegistration()
         {
             return View();
@@ -25,5 +46,15 @@ namespace BusinessTrips.Controllers
         {
             return View();
         }
+
+        /*
+         int l=0,O=1;
+         
+         if(l==0)
+            O=1;
+         else
+            O=0;
+         
+         */ 
 	}
 }
