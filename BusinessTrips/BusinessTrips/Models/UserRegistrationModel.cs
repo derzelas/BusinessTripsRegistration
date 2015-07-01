e28bdfd4af712e226436b6f0c11c06a5844a562d
sandica <sandica_robert@yahoo.ro>
2015-07-01 11:51:27 +0300

    <email_1>

--- BusinessTrips/BusinessTrips/Models/UserRegistrationModel.cs
+++ BusinessTrips/BusinessTrips/Models/UserRegistrationModel.cs
@@ -1,4 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Mail;
+using BusinessTrips.DataAccessLayer;
+
+//e de la e mail
 
 namespace BusinessTrips.Models
 {
@@ -6,7 +11,6 @@ namespace BusinessTrips.Models
     {
         private const int MinimumPasswordLength = 6;
         private const int MinimumNameLength = 3;
-
         private const string PasswordValidationMessage = "Minimum password length is 6";
 
         [Required]
@@ -31,5 +35,34 @@ namespace BusinessTrips.Models
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         [Display(Name = "Confirm password")]
         public string ConfirmedPassword { get; set; }
+
+        public void Save(UserRegistrationModel userFields)
+        {
+            IStorage<UserRegistrationModel> storage = new InMemoryStorage<UserRegistrationModel>();
+
+            var registrationRepository = new UserRegistrationRepository(storage);
+            registrationRepository.Add(userFields);
+            Email a = new Email();
+            a.Send();
+        }
+    }
+
+    public class Email
+    {
+        public void Send()
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587);
+            var message = new MailMessage();
+            message.From = new MailAddress("thezohan123456789@gmail.com");
+            message.To.Add("petrica.bota2@yahoo.com");
+            message.Body = "Hello World";
+            message.Subject = "hope this works";
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = true;
+
+            client.Credentials = new NetworkCredential("thezohan123456789@gmail.com", "FieruBateOsu123!@#");
+            client.Send(message);
+            message = null;
+        }
     }
 }
