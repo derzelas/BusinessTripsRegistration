785c9ca732fe3ccd47b2ad6ba0dd5ffef417b323
Andreea19 <nicoara.andreea19@gmail.com>
2015-07-01 14:22:27 +0300

    Added modifications to Login view

---
+++ BusinessTrips/BusinessTrips/db.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BusinessTrips.DataAccessLayer;
+using BusinessTrips.Models;
+
+namespace BusinessTrips
+{
+    public static class db
+    {
+        public static IStorage<UserRegistrationModel> storage = new InMemoryStorage<UserRegistrationModel>();
+    }
+}
