f66ec319cd9dced645136ab4d16ce696e6122f9a
sandica <sandica_robert@yahoo.ro>
2015-07-02 13:36:35 +0300

    create tests folder dor UserRegistrationModel

---
+++ BusinessTrips/BusinessTrips.Tests/Models/UserRegistrationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessTrips.Tests.Models
+{
+    [TestClass]
+    public class UserRegistrationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
