28feb692369fd768c398b3caa9d417f7eea16c70
BotaPetrica <petrica.bota2@yahoo.com>
2015-07-10 15:08:56 +0300

    view principal gata

--- BusinessTrips/BusinessTrips.DAL/Model/BusinessTripModel.cs
+++ BusinessTrips/BusinessTrips.DAL/Model/BusinessTripModel.cs
@@ -9,10 +9,14 @@ namespace BusinessTrips.DAL.Model
         public UserModel User { get; set; }
 
         [Required]
+        [Display(Name = "Department:")]
+        public DepartmentType Department { get; set; }
+
+        [Required]
         [Display(Name = "PM name:")]
         public string PmName { get; set; }
 
-        [Required]
+        
         [Display(Name = "Project number:")]
         public string ProjectNumber { get; set; }
 
@@ -22,17 +26,13 @@ namespace BusinessTrips.DAL.Model
         [Display(Name = "Task name:")]
         public string TaskName { get; set; }
 
-        [Required]
+        
         [Display(Name = "Task number:")]
         public string TaskNumber { get; set; }
 
         [Required]
-        [Display(Name = "Department:")]
-        public DepartmentType Department { get; set; }
-
-        [Required]
         [Display(Name = "Leaving from:")]
-        public string LeavingFrom { get; set; }
+        public LeavingLocation LeavingFrom { get; set; }
 
         [Required]
         [Display(Name = "Starting date:")]
@@ -50,8 +50,9 @@ namespace BusinessTrips.DAL.Model
         [Display(Name = "Client location:")]
         public string ClientLocation { get; set; }
 
+        [Display(Name = "With personal car")]
         public bool WithPersonalCar { get; set; }
-        public string MeanOfTransportation { get; set; }
+        public string MeansOfTransportation { get; set; }
 
         [Display(Name = "Accomodation:")]
         public string Accomodation { get; set; }
@@ -62,7 +63,7 @@ namespace BusinessTrips.DAL.Model
         [Display(Name = "Bank card is needed:")]
         public bool BankCardIsNeeded { get; set; }
 
-        [Display(Name = "Anything else to consider important:")]
+        [Display(Name = "Anything else you consider important:")]
         public string OtherInfo { get; set; }
 
         public string Status { get; set; }
@@ -72,6 +73,12 @@ namespace BusinessTrips.DAL.Model
             Department1 = 0,
             Department2,
             Department3
+        }
+        public enum LeavingLocation
+        {
+            Sibiu = 0,
+            Cluj,
+            Iasi
         }
     }
 }
