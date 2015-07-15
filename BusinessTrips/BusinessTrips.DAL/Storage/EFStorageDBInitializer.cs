09167f64169e8e4e4bb8c78c990aa01295674d34
alexandru_matei2000@yahoo.com <princeofpersia28>
2015-07-15 13:58:48 +0300

    Added RoleRepository and small changes to roles

--- BusinessTrips/BusinessTrips.DAL/Storage/EFStorageDBInitializer.cs
+++ BusinessTrips/BusinessTrips.DAL/Storage/EFStorageDBInitializer.cs
@@ -1,8 +1,16 @@
 ﻿using System.Data.Entity;
+using BusinessTrips.DAL.Entity;
 
 namespace BusinessTrips.DAL.Storage
 {
     public class EfStorageDbInitializer : DropCreateDatabaseIfModelChanges<EfStorage>
     {
+        protected override void Seed(EfStorage context)
+        {
+            context.Roles.Add(new RoleEntity {Name = "Regular"});
+            context.Roles.Add(new RoleEntity {Name = "HR"});
+
+            base.Seed(context);
+        }
     }
 }
