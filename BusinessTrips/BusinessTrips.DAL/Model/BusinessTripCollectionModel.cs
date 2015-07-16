2f5000dbf7089f24b02921813cfe39ec60156993
Dragos <hodina.dragos@gmail.com>
2015-07-16 15:46:32 +0300

    Added small changes to BusinessTripcollectionModel

--- BusinessTrips/BusinessTrips.DAL/Model/BusinessTripCollectionModel.cs
+++ BusinessTrips/BusinessTrips.DAL/Model/BusinessTripCollectionModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using BusinessTrips.DAL.Repository;
 
 namespace BusinessTrips.DAL.Model
@@ -9,13 +8,13 @@ namespace BusinessTrips.DAL.Model
     {
         public void LoadBusinessTripForUser(Guid userId)
         {
-            BusinessTripsRepository businessTripsRepository = new BusinessTripsRepository();
+            var businessTripsRepository = new BusinessTripsRepository();
             //BusinessTripModels = businessTripsRepository.GetByUser(userId);
         }
 
         public IEnumerable<SearchBusinessTripModel> LoadOtherBusinessTrips(BusinessTripFilter filter)
         {
-            BusinessTripsRepository businessTripsRepository = new BusinessTripsRepository();
+            var businessTripsRepository = new BusinessTripsRepository();
             return businessTripsRepository.GetOtherBusinessTrips(filter);
         }
     }
