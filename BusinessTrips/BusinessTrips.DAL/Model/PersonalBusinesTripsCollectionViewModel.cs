6dc081620b1577363b8833be968bf88873fba661
Dragos <hodina.dragos@gmail.com>
2015-07-20 13:31:50 +0300

    added the .cs file (was in untracked)

---
+++ BusinessTrips/BusinessTrips.DAL/Model/PersonalBusinesTripsCollectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BusinessTrips.DAL.Model
+{
+    public class PersonalBusinesTripsCollectionViewModel
+    {
+        public IEnumerable<PersonalBusinesTripsViewModel> MyBusinesTripsViewModels { get; set; }
+
+        public PersonalBusinesTripsCollectionViewModel()
+        {
+            MyBusinesTripsViewModels = new List<PersonalBusinesTripsViewModel>();
+        }
+    }
+}
