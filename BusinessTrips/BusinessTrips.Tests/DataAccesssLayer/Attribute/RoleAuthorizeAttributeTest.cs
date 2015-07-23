2c755ab5aeafe62d5b9fd02aee048c64c9643880
alexandru_matei2000@yahoo.com <princeofpersia28>
2015-07-23 16:13:52 +0300

    Role Authorize Attribute Test

---
+++ BusinessTrips/BusinessTrips.Tests/DataAccesssLayer/Attribute/RoleAuthorizeAttributeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using BusinessTrips.DAL.Attribute;
+using BusinessTrips.DAL.Storage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessTrips.Tests.DataAccesssLayer.Attribute
+{
+    [TestClass]
+    public class RoleAuthorizeAttributeTest
+    {
+        [TestMethod]
+        public void Constructor_MultipleParameters_MergesThemSeparatedByCommaInSingleString()
+        {
+            RoleAuthorizeAttribute authorizeAttribute = new RoleAuthorizeAttribute(Role.Regular, Role.Hr);
+
+            Assert.AreEqual("Regular,Hr", authorizeAttribute.Roles);
+        }
+    }
+}
