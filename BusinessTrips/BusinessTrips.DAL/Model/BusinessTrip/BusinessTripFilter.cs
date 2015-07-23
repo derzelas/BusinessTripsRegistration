58404ce0684e69440970d8890bc95b7f84181bca
sandica <sandica_robert@yahoo.ro>
2015-07-23 16:01:34 +0300

    refactor bussines trip repositori and filtermodel

--- BusinessTrips/BusinessTrips.DAL/Model/BusinessTrip/BusinessTripFilter.cs
+++ BusinessTrips/BusinessTrips.DAL/Model/BusinessTrip/BusinessTripFilter.cs
@@ -13,27 +13,20 @@ namespace BusinessTrips.DAL.Model.BusinessTrip
 
         public Guid? Guid { get; set; }
 
-        [DisplayName("Name")]
-        public string Person { get; set; }
-
-        [DisplayName("Client Name")]
-        public string ClientName { get; set; }
+        [DisplayName("Staring Date")]
+        [DataType(DataType.Date)]
+        public DateTime? StartingDate { get; set; }
 
         [DisplayName("Location")]
         public string Location { get; set; }
+
+        [DisplayName("Person")]
+        public string Person { get; set; }
 
         [DisplayName("Mean Of Transporation")]
         public string MeanOfTransportation { get; set; }
 
         [DisplayName("Accomodation")]
-        public string Accommodation { get; set; }
-
-        [DisplayName("Staring Date")]
-        [DataType(DataType.Date)]
-        public DateTime? StartingDate { get; set; }
-
-        [DisplayName("Ending Date")]
-        [DataType(DataType.Date)]
-        public DateTime? EndingDate { get; set; }
+        public string Accommodation { get; set; }    
     }
 }
--- BusinessTrips/BusinessTrips.DAL/Repository/BusinessTripsRepository.cs
+++ BusinessTrips/BusinessTrips.DAL/Repository/BusinessTripsRepository.cs
@@ -60,14 +60,9 @@ namespace BusinessTrips.DAL.Repository
                         m => m.Status == BusinessTripStatus.Accepted || m.Status == BusinessTripStatus.Pending);
             }
 
-            if (!string.IsNullOrEmpty(filter.Person))
+            if (filter.StartingDate.HasValue)
             {
-                businessTrips = businessTrips.Where(m => m.User.Name.Contains(filter.Person));
-            }
-
-            if (!string.IsNullOrEmpty(filter.ClientName))
-            {
-                businessTrips = businessTrips.Where(m => m.ClientName.Contains(filter.ClientName));
+                businessTrips = businessTrips.Where(m => m.StartingDate == filter.StartingDate);
             }
 
             if (!string.IsNullOrEmpty(filter.Location))
@@ -75,9 +70,9 @@ namespace BusinessTrips.DAL.Repository
                 businessTrips = businessTrips.Where(m => m.ClientLocation.Contains(filter.Location));
             }
 
-            if (!string.IsNullOrEmpty(filter.Accommodation))
+            if (!string.IsNullOrEmpty(filter.Person))
             {
-                businessTrips = businessTrips.Where(m => m.Accomodation.Contains(filter.Accommodation));
+                businessTrips = businessTrips.Where(m => m.User.Name.Contains(filter.Person));
             }
 
             if (!string.IsNullOrEmpty(filter.MeanOfTransportation))
@@ -85,14 +80,9 @@ namespace BusinessTrips.DAL.Repository
                 businessTrips = businessTrips.Where(m => m.MeansOfTransportation.Contains(filter.MeanOfTransportation));
             }
 
-            if (filter.StartingDate.HasValue)
+            if (!string.IsNullOrEmpty(filter.Accommodation))
             {
-                businessTrips = businessTrips.Where(m => m.StartingDate == filter.StartingDate);
-            }
-
-            if (filter.EndingDate.HasValue)
-            {
-                businessTrips = businessTrips.Where(m => m.EndingDate == filter.EndingDate);
+                businessTrips = businessTrips.Where(m => m.Accomodation.Contains(filter.Accommodation));
             }
 
             return businessTrips;
