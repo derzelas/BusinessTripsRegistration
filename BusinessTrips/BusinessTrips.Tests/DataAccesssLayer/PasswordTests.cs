d7868b02289657396cd849a6201261abb3928bb5
gabriel.tudorica@ymail.com <gabriel.tudorica@ymail.com>
2015-07-23 12:34:18 +0300

    Added password class with tests .To be used for replacing RandomSaltGenerator & PasswordHasher

---
+++ BusinessTrips/BusinessTrips.Tests/DataAccesssLayer/PasswordTests.cs
@@ -0,0 +1,48 @@
+﻿using BusinessTrips.DAL.Model.User;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessTrips.Tests.DataAccesssLayer
+{
+    [TestClass]
+    public class PasswordTests
+    {
+        private const string ClearTextPassword = "password";
+        private const string Salt = "salt";
+
+        [TestMethod]
+        public void GetHashed_IsDifferentThanClearPassword()
+        {
+            var password = new Password(ClearTextPassword, "salt");
+
+            Assert.AreNotEqual(ClearTextPassword, password.GetHashed());
+        }
+
+        [TestMethod]
+        public void GetSalt_ReturnsUsedSalt()
+        {                       
+            var password = new Password(ClearTextPassword, Salt);
+
+            string actualSalt = password.GetSalt();
+
+            Assert.AreEqual(Salt, actualSalt);
+        }
+
+        [TestMethod]
+        public void GetHashed_SamePasswordDifferentSalt_ReturnsDifferentHashedPassword()
+        {
+            var hashedPasswordOne = new Password(ClearTextPassword, Salt).GetHashed();
+            var hashedPasswordTwo = new Password(ClearTextPassword, Salt + "2").GetHashed();
+
+            Assert.AreNotEqual(hashedPasswordOne, hashedPasswordTwo);
+        }
+
+        [TestMethod]
+        public void GetSalt_ReturnsUniqueSaltPerInstance()
+        {
+            var passwordOne = new Password(ClearTextPassword);
+            var passwordTwo = new Password(ClearTextPassword);
+
+            Assert.AreNotEqual(passwordOne.GetSalt(), passwordTwo.GetSalt());
+        }
+    }
+}
